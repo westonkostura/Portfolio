7507a1e3e284b9d89b491d12f5413a61928a7f67
Weston Kostura <weston.kostura@gmail.com>
2023-12-06 01:20:33 -0700

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import UserProvider from './utils/UserContext';
-import UserComponent from './components/UserComponent';
-import './App.css';
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
 
-export default function App() {
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <div>
-      <UserProvider>
-        <UserComponent />
-      </UserProvider>
-    </div>
+    <>
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
   );
 }
+
+export default App;
